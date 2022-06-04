31e7da5e176fe5fd475b2f5b54f47fcc8f809bc6
kfisch2 <kmlindsey24@gmail.com>
2022-06-04 10:37:38 -0700

    display current day/date

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
+// current day; date displays when page is completely loaded
 $(document).ready(function(){
   let date = new Date();
-    let value = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-    $("#showDate").text(value);
-  });			
+  let value = `${["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][date.getDay()]}, ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  $("#showDate").text(value)
+});			
+
