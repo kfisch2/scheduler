efbdd007bb7f226ef1a8bb2e195f06076bed8040
kfisch2 <kmlindsey24@gmail.com>
2022-06-07 12:14:37 -0700

    save event to localStorage 9AM

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,71 @@
 // current day/date
-let setTime = () => {
+let currentHour = () => {
   let date = moment().format("MMMM Do YYYY, h:mm:ss a");
-  $(document).ready(function(){
   $("#showDate").text(date);
-  })
 };
 
-setTime();
-
-// refresh page every second
-setInterval(setTime, 1000)
+// display current time every second
+setInterval(currentHour, 1000)
 
 
-
-// user input in rows
-let input = document.querySelectorAll(".input");
-for(i=0; i < input.length; i++) {
-  input[i].innerHTML = '<input type="text" name="task" placeholder="Enter event here" style="width:50" >'
-};
-
-// hour and event variables
+// hour, event, and save button variables
 let hour = document.querySelectorAll(".hour");
 let userInput = document.querySelectorAll(".input")
+let saveBtn = document.querySelectorAll(".saveBtn")
+
 
 // hour block function
-function timeBlock(startTime) { 
-  for(i=0; i < hour.length; i++ ) {
-    hour[i].innerHTML = startTime++ ;
+function timeBlock(startTime) {
+  for (i = 0; i < hour.length; i++) {
+    hour[i].innerHTML = startTime++;
   };
 };
 
 // change background color depending on time of day
-function changeBackgroundColor (event) {
-  if(hour[event].innerHTML > new Date().getHours()) {
+function changeBackgroundColor(event) {
+  if (hour[event].innerHTML > new Date().getHours()) {
     userInput[event].classList.add("future")
   } else if (hour[event].innerHTML < new Date().getHours()) {
-  userInput[event].classList.add("past")
+    userInput[event].classList.add("past")
   } else {
-  userInput[event].classList.add("present")
+    userInput[event].classList.add("present")
   }
 };
 
 // iterate through each hour
-function hourOfDay () {
-  for (i=0; i < 9; i++) {
-  changeBackgroundColor(i);
+function hourOfDay() {
+  for (i = 0; i < 9; i++) {
+    changeBackgroundColor(i);
   }
 };
 
-// save userInput to localStorage
+// add user event to hour blocks
+function renderTask() {
+
+  // set input/output for 9AM
+  let userInput9AM = document.getElementById("9AM")
+  let userOutput9AM = document.getElementById("9AM");
+  let nineAMSaveBtn = document.getElementById("9AMBtn");
+
+  // set textContent to local storage
+  function storeEvent() {
+    localStorage.setItem("9AM", userInput9AM.value);
+    userInput.textContent = userInput9AM.value;
+  };
+
+  // retrieve textContent from local storage
+  userOutput9AM.textContent = localStorage.getItem("9AM");
+  userInput9AM.value = localStorage.getItem("9AM");
+
+  // when 9AM save button clicked, 
+  nineAMSaveBtn.addEventListener("click", storeEvent);
+};
 
 
-
-
-
-timeBlock(08);
+renderTask();
+currentHour();
+timeBlock(09);
 hourOfDay();
+
+
+
